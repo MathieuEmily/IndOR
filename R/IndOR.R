85c9881ebc506c715c241fa40ce0909c15129377
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-07-22 16:41:01 +0200

    a

--- R/IndOR.R
+++ R/IndOR.R
@@ -1,16 +1,16 @@
-IndOR <- function(M) ##### M is a 9x2 table where the first column is made by counts for the nine genotypes in cases and the second column stores counts for controls
+IndOR <- function(x) 
 {
-	B <- GetBLog(M)
+	B <- GetBLog(x)
 	VarCov <- matrix(-1,ncol=4,nrow=4)
 	for (i in 1:4)
 	{	
-		VarCov[i,i] <- VarDelta(M[,1],ind=i)+VarDelta(M[,2],ind=i)
+		VarCov[i,i] <- VarDelta(x[,1],ind=i)+VarDelta(x[,2],ind=i)
 	}
 	for (i in 1:3)
 	{
 		for (j in (i+1):4)
 		{
-			tmp <- CovDelta(M[,1],ind1=i,ind2=j)+CovDelta(M[,2],ind1=i,ind2=j)
+			tmp <- CovDelta(x[,1],ind1=i,ind2=j)+CovDelta(x[,2],ind1=i,ind2=j)
 			VarCov[i,j] <- tmp
 			VarCov[j,i] <- tmp
 		}
